945f976317858303ce1627d27ed758e9f7900ee8
Aman Sharma <as7500257655@gmail.com>
2025-08-08 01:47:56 +0000

    test2

---
+++ .vscode/Is_prime.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cout<<"Enter the number : ";
+    cin>>N;
+    for(int i = 2; i<N ; i++){
+        if (N%i == 0 ){
+            cout<<"Not Prime";
+            break;
+        }
+        else{
+            cout<<"Prime";
+            break;
+        }
+    }
+return 0;
+}
